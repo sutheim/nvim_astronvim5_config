ffb7956ba1d625c135c0bfc2b2282e282081356c
sutheim <sondreutheim@gmail.com>
2025-12-21 02:32:05 +0100

    enable format on save for rust

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -18,9 +18,9 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "rust",
+          "rust",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
